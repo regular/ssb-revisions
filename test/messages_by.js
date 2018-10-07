68b5be739f492b7690961a1d810ab8ef2a291255
Jan Bölsche <regular.gonzales@googlemail.com>
2018-10-07 13:37:08 +0200

    review-level updates seq even when map returns same entries

--- test/messages_by.js
+++ test/messages_by.js
@@ -19,16 +19,19 @@ function append(db, msgs, cb) {
 }
 
 function run_test(prop, fun) {
-  test('simple', (t, db) => {
+  test(fun, (t, db) => {
     const keyA = rndKey()
     const keyA1 = rndKey()
+    const keyA2 = rndKey()
     const a = msg(keyA)
     const a1 = msg(keyA1, keyA, [keyA])
+    const a2 = msg(keyA2, keyA, [keyA1])
 
     a.value.content[prop] = 'dog'
-    a1.value.content[prop] = 'cat'
+    a1.value.content[prop] = 'dog'
+    a2.value.content[prop] = 'cat'
 
-    const done = multicb()
+    const done = multicb({pluck: 1})
     const cb1 = done()
     const cb2 = done()
 
@@ -36,29 +39,35 @@ function run_test(prop, fun) {
       db.revisions[fun]('cat', {live: true, sync:false}),
       pull.take(1),
       pull.collect( (err, results) => {
+        if (err) return cb1(err)
         console.log('cat results', results)
-        t.deepEqual(results[0].key, ['cat', keyA1])
-        t.deepEqual(results[0].value, a1)
+        t.deepEqual(results[0].key, ['cat', keyA])
+        t.deepEqual(results[0].value, a2)
         cb1()
       })
     )
 
     pull(
       db.revisions[fun]('dog', {live: true, sync: false}),
-      pull.take(2),
+      pull.take(3),
       pull.collect( (err, results) => {
+        if (err) return cb2(err)
         console.log('dog results', results)
         t.deepEqual(results[0].key, ['dog', keyA])
         t.deepEqual(results[0].value, a)
-        t.deepEqual(results[0].key, ['dog', keyA])
-        t.equal(results[1].type, 'del')
+        t.deepEqual(results[1].key, ['dog', keyA])
+        t.deepEqual(results[1].value, a1)
+        t.deepEqual(results[2].key, ['dog', keyA])
+        t.equal(results[2].type, 'del')
         cb2()
       })
     )
 
-    append(db, [a, a1], done())
+    append(db, [a, a1, a2], ()=>{})
 
-    done( ()=>{
+    done( err =>{
+      if (err) console.log(err)
+      console.log('done')
       t.end()
     })
   })
